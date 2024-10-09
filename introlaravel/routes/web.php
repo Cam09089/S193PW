487a8a21c5649725760ae60b16c209c9af210eef
Cam09089 <perana809@gmail.com>
2024-10-08 20:26:25 -0600

    avance practica 3.1

--- introlaravel/routes/web.php
+++ introlaravel/routes/web.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::view('/','inicio')->name('rutainicio');
 Route::view('/form','formulario')->name('rutaform');
 Route::view('/consultar','clientes')->name('rutaclientes');
+
+Route::view('/component','componentes')->name('rutacomponent');
