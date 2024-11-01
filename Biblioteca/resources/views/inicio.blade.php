b1d74463bcf0d059886f347af77427c3e53f5ff5
Cam09089 <perana809@gmail.com>
2024-10-31 23:21:18 -0600

    Mensajes de inputs y vistas disponible en Inglés y español

--- Biblioteca/resources/views/inicio.blade.php
+++ Biblioteca/resources/views/inicio.blade.php
@@ -10,17 +10,17 @@
 </head>
 <body>
     <div class="full-height">
-    <h1 class="display-2">Bienvenido a tu Biblioteca Virtual!</h1>
+    <h1 class="display-2">{{__('Bienvenido a tu Biblioteca Virtual!')}}</h1>
 
-    <p>Presiona para comenzar el registro</p>
+    <p>{{__('Presiona para comenzar el registro')}}</p>
 
-    <a href="{{ route('rutaregistrolibro') }}" class="btn btn-custom btn-lg mt-3">Ir al registro</a>
+    <a href="{{ route('rutaregistrolibro') }}" class="btn btn-custom btn-lg mt-3">{{__('Ir al registro')}}</a>
     </div>
 
   <center> <div class="noti-section mt-4">
-        <h2 class="noti-titulo">Noticia Literaria</h2>
+        <h2 class="noti-titulo">{{__('Noticia Literaria')}}</h2>
 
-        <p class="noti-content">24 de Octubre. Dia de las Bibliotecas</p>
+        <p class="noti-content">{{__('24 de Octubre. Dia de las Bibliotecas')}}</p>
         <img src="{{ asset('img/noticia.png') }}" alt="yo" width="400">
 
     </div></center> 
@@ -28,7 +28,7 @@
     <footer class="footer mt-auto py-3">
         <div class="container text-center">
             <p class="nombre-libro"></p>
-            <p>&copy; {{ date('Y') }} Biblioteca </p>
+            <p>&copy; {{ date('Y') }} {{__('Biblioteca')}} </p>
             <p>{{ date('d') }} de {{ date('F') }}, {{ date('Y') }}</p>
 
         </div>
--- Biblioteca/resources/views/registrolibro.blade.php
+++ Biblioteca/resources/views/registrolibro.blade.php
@@ -23,54 +23,54 @@
 
     <div class="card-body text-justify ">
 
-    <h2 class="text-center mb-4">Registro</h2>
+    <h2 class="text-center mb-4">{{__('Registro')}}</h2>
 
 <form action="/enviardatos" method="POST">
     @csrf
      <div class="mb-3">
-    <label for="isbn" class="form-label">ISBN</label>
+    <label for="isbn" class="form-label">{{__('ISBN')}}</label>
     <input type="text" class="form-control" name="txtisbn">
     <small>{{ $errors->first('txtisbn') }}</small>
     </div>
 
     <div class="mb-3">
-    <label for="titulo" class="form-label">Titulo</label>
+    <label for="titulo" class="form-label">{{__('Titulo')}}</label>
     <input type="text" class="form-control" name="txttitulo" value="{{ old('txttitulo') }}">
     <small class="text-danger fst-italic">{{ $errors->first('txttitulo') }}</small>
 </div>
 
     <div class="mb-3">
-    <label for="autor" class="form-label">Autor</label>
+    <label for="autor" class="form-label">{{__('Autor')}}</label>
     <input type="text" class="form-control" name="txtautor">
     <small>{{ $errors->first('txtautor') }}</small>
     </div>
 
     <div class="mb-3">
-    <label for="paginas" class="form-label">Paginas</label>
+    <label for="paginas" class="form-label">{{__('Paginas')}}</label>
     <input type="text" class="form-control" name="txtpaginas">
     <small>{{ $errors->first('txtpaginas') }}</small>
     </div>
 
     <div class="mb-3">
-    <label for="anio" class="form-label">Año</label>
+    <label for="anio" class="form-label">{{__('Año')}}</label>
     <input type="text" class="form-control" name="txtanio">
     <small>{{ $errors->first('txtanio') }}</small>
     </div>
 
     <div class="mb-3">
-    <label for="editorial" class="form-label">Editorial</label>
+    <label for="editorial" class="form-label">{{__('Editorial')}}</label>
     <input type="text" class="form-control" name="txteditorial">
     <small>{{ $errors->first('txteditorial') }}</small>
     </div>
 
     <div class="mb-3">
-    <label for="correo" class="form-label">Correo de la Editorial</label>
+    <label for="correo" class="form-label">{{__('Correo de la Editorial')}}</label>
     <input type="text" class="form-control" name="txtcorreo">
     <small>{{ $errors->first('txtcorreo') }}</small>
     </div>
 
     <div>
-    <button type="submit" class="btn btn-primary">Enviar</button>
+    <button type="submit" class="btn btn-primary">{{__('Enviar')}}</button>
     </div>
 
      </div>
@@ -81,7 +81,7 @@
 <footer class="footer mt-auto py-3">
         <div class="container1 text-center">
             <p class="nombre-libro"></p>
-            <p>&copy; {{ date('Y') }} Biblioteca </p>
+            <p>&copy; {{ date('Y') }} {{__('Biblioteca')}} </p>
             <p>{{ date('d') }} de {{ date('F') }}, {{ date('Y') }}</p>
 
         </div>
